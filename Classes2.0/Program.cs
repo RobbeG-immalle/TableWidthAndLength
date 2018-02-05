b6860e19c48b4e74630df085e692cdb6319c1cf5
Robbe Gillis <robbe.gillis@immalle.eu>
2018-02-05 09:29:34 +0100

    Met List

--- Classes2.0/Program.cs
+++ Classes2.0/Program.cs
@@ -10,6 +10,20 @@ namespace Classes2._0
     {
         static void Main(string[] args)
         {
+            List<Tafel> tafels = new List<Tafel>();
+            Random rndGen = new Random();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var w = rndGen.Next(50, 200);
+                var h = rndGen.Next(50, 200);
+                tafels.Add(new Tafel(w, h));
+            }
+            foreach (var table in tafels)
+            {
+                table.ShowData();
+            }
         }
+
     }
 }
---
+++ Classes2.0/Tafel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2._0
+{
+    class Tafel
+    {
+        private int width;
+        private int height;
+        public Tafel(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine("The Width of the table is {0} and the height is {1}", width,height);
+        }
+    }
+
+}
